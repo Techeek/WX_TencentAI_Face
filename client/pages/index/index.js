4e2e50c439eee6feb83e74700ae5c4cf08148bce
Techeek <qc_957a692741d9444b9f25c53a3da69e7a@git.code.tencent.com>
2018-12-17 11:55:48 +0800

    增加备注

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -19,43 +19,44 @@ Page({
             wx.hideLoading({
               title: '加载中',
             })
-            var obj = JSON.parse(res.data)
-            var ctx_size = 250 / obj.data.image_height
-            ctx.drawImage(tempFilePaths[0], 0, 0, obj.data.image_height * ctx_size, obj.data.image_width * ctx_size);
+            var obj = JSON.parse(res.data) //将返回的数值转换为json
+            var ctx_size = 250 / obj.data.image_height;
+            // 获取图片与canvas的比值
+            ctx.drawImage(tempFilePaths[0], 0, 0, obj.data.image_height * ctx_size, obj.data.image_width * ctx_size); //显示上传的照片内容，并乘以比例正常显示
             ctx.setStrokeStyle('red')
-            ctx.strokeRect(obj.data.face[0].x * ctx_size, obj.data.face[0].y * ctx_size, obj.data.face[0].height * ctx_size, obj.data.face[0].width * ctx_size)
+            ctx.strokeRect(obj.data.face[0].x * ctx_size, obj.data.face[0].y * ctx_size, obj.data.face[0].height * ctx_size, obj.data.face[0].width * ctx_size) //绘制人脸识别的矩形
             ctx.beginPath();
             ctx.moveTo(obj.data.face[0].face_shape.face_profile[0].x * ctx_size, obj.data.face[0].face_shape.face_profile[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.face_profile.length;i++){
               ctx.lineTo(obj.data.face[0].face_shape.face_profile[i].x * ctx_size, obj.data.face[0].face_shape.face_profile[i].y * ctx_size)
-            }
+            } //绘制脸部轮廓
             ctx.moveTo(obj.data.face[0].face_shape.left_eye[0].x * ctx_size, obj.data.face[0].face_shape.left_eye[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.left_eye.length; i++) {
               ctx.lineTo(obj.data.face[0].face_shape.left_eye[i].x * ctx_size, obj.data.face[0].face_shape.left_eye[i].y * ctx_size)
-            }
+            } //绘制左眼轮廓
             ctx.moveTo(obj.data.face[0].face_shape.right_eye[0].x * ctx_size, obj.data.face[0].face_shape.right_eye[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.right_eye.length; i++) {
               ctx.lineTo(obj.data.face[0].face_shape.right_eye[i].x * ctx_size, obj.data.face[0].face_shape.right_eye[i].y * ctx_size)
-            }
+            } //绘制右眼轮廓
             ctx.moveTo(obj.data.face[0].face_shape.left_eyebrow[0].x * ctx_size, obj.data.face[0].face_shape.left_eyebrow[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.left_eyebrow.length; i++) {
               ctx.lineTo(obj.data.face[0].face_shape.left_eyebrow[i].x * ctx_size, obj.data.face[0].face_shape.left_eyebrow[i].y * ctx_size)
-            }
+            } //绘制左边眉毛轮廓
             ctx.moveTo(obj.data.face[0].face_shape.right_eyebrow[0].x * ctx_size, obj.data.face[0].face_shape.right_eyebrow[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.right_eyebrow.length; i++) {
               ctx.lineTo(obj.data.face[0].face_shape.right_eyebrow[i].x * ctx_size, obj.data.face[0].face_shape.right_eyebrow[i].y * ctx_size)
-            }
+            } //绘制右边眉毛轮廓
             ctx.moveTo(obj.data.face[0].face_shape.mouth[0].x * ctx_size, obj.data.face[0].face_shape.mouth[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.mouth.length; i++) {
               ctx.lineTo(obj.data.face[0].face_shape.mouth[i].x * ctx_size, obj.data.face[0].face_shape.mouth[i].y * ctx_size)
-            }
+            } //绘制嘴部轮廓
             ctx.moveTo(obj.data.face[0].face_shape.nose[0].x * ctx_size, obj.data.face[0].face_shape.nose[0].y * ctx_size)
             for (var i = 1; i < obj.data.face[0].face_shape.nose.length; i++) {
               ctx.lineTo(obj.data.face[0].face_shape.nose[i].x * ctx_size, obj.data.face[0].face_shape.nose[i].y * ctx_size)
-            }
+            } //绘制鼻子轮廓
             ctx.stroke();
             ctx.draw()
-            switch(true){
+            switch(true){ //遮挡检测
               case obj.data.face[0].mask <30:
                 myThis.setData({
                   mask:"没有遮挡"
@@ -72,19 +73,19 @@ Page({
                 });
                 break;
             }
-            switch (true) {
+            switch (true) { //性别检测
               case obj.data.face[0].gender < 50:
                 myThis.setData({
                   gender: "女"
                 });
                 break;
-              case obj.data.face[0].gender > 60:
+              case obj.data.face[0].gender > 50:
                 myThis.setData({
                   gender: "男"
                 });
                 break;
             }
-            switch (true) {
+            switch (true) { //帽子检测
               case obj.data.face[0].hat < 30:
                 myThis.setData({
                   hat: "没有戴帽子"
@@ -101,7 +102,7 @@ Page({
                 });
                 break;
             }
-            switch (obj.data.face[0].glasses) {
+            switch (obj.data.face[0].glasses) { //眼镜检测
               case 1:
                 myThis.setData({
                   glasses: "戴眼镜"
@@ -114,14 +115,14 @@ Page({
                 break;
             }
             myThis.setData({
-              age: obj.data.face[0].age,
-              beauty: obj.data.face[0].beauty,
+              age: obj.data.face[0].age, //年龄
+              beauty: obj.data.face[0].beauty, //颜值
             })
           }
         })
         uploadTask.onProgressUpdate((res) => {
           myThis.setData({
-            progress: res.progress
+            progress: res.progress //上传进度
           })
         })
       }
@@ -144,7 +145,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const ctx = wx.createCanvasContext('myCanvas')
+    const ctx = wx.createCanvasContext('myCanvas') //绘制基本图形
     const image = "../../lib/img/imgsrc.jpg";
     ctx.drawImage(image, 0, 0, 250, 250);
     ctx.draw()
